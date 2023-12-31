d0680b6631854c104a5379e9d5aa5a21ccb802b4
Gabriela França <gabrielafranca2008@hotmail.com>
2023-12-31 16:03:53 -0300

    usando closures

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -13,7 +13,9 @@ export function Comment({content, OnDeleteComment}) {
   }
 
   function handleLikeComment() {
-    setLikeCount(likeCount + 1)
+    setLikeCount((state) => {
+      return state + 1
+    })
   }
 
   return (
